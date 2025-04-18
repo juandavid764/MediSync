eaf006547b30ed257f410acf3ae17eedbf2fbb66
Maria Camila Orozco <mcor2437@gmail.com>
2025-04-17 22:53:03 -0500

    funciona, pero las pruebas unitarias del login estan malas - toca corregir

--- src/supabase/client.js
+++ src/supabase/client.js
@@ -1,8 +1,11 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://your-supabase-url.supabase.co';
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'your-anon-key';
+
+console.log("Supabase URL:", supabaseUrl);
+console.log("Supabase Key:", supabaseKey);
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-})
+
+
+export default defineConfig((props) => {
+  const env = loadEnv(props.mode, process.cwd(), "VITE_APP");
+  const envWithProcessPrefix = {
+    "process.env": `${JSON.stringify(env)}`,
+  };
+
+  return {
+      plugins: [
+          react(),tailwindcss(),EnvironmentPlugin("all")
+      ],
+      define: envWithProcessPrefix,
+     }
+  })
